c78157f29eaf00d21bac2c0e350b5c5131788813
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-31 01:59:54 +0530

    Created Stack Navigator for map screen

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -2,7 +2,9 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react'
 import { FlatList, Image, TouchableOpacity, View, Text } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames'
+import { selectOrigin } from '../../slices/navSlice';
 
 const data = [
     {
@@ -22,6 +24,7 @@ const data = [
 const NavOptions = () => {
 
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
 
     return (
         <FlatList 
@@ -31,8 +34,9 @@ const NavOptions = () => {
             renderItem={({item}) => (
                 <TouchableOpacity 
                     onPress={() => navigation.navigate(item.screen)}
+                    disabled={!origin}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                    <View>
+                    <View style={tw`${!origin && "opacity-20"}`}>
                         <Image 
                             source={{
                                 uri: item.image
