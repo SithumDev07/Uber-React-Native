1d3dd960bb61b41db7ebc7211db193867a59ed90
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-31 03:42:37 +0530

    Added RideOptionsCard

--- src/components/NavigateCard.js
+++ src/components/NavigateCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import tailwind from 'tailwind-react-native-classnames'
 import { GOOGLE_MAPS_APIKEY } from '@env'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { useDispatch } from 'react-redux'
 import { setDestination } from '../../slices/navSlice'
 import { useNavigation } from '@react-navigation/core'
+import NavFavourites from './NavFavourites'
+import { Icon } from 'react-native-elements'
 
 const NavigateCard = () => {
 
@@ -39,6 +41,30 @@ const NavigateCard = () => {
                         enablePoweredByContainer={false}
                     />
                 </View>
+
+                <NavFavourites />
+            </View>
+
+            <View style={tailwind`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                <TouchableOpacity style={tailwind`flex flex-row bg-black justify-between w-24 px-4 py-3 rounded-full`} onPress={() => navigation.navigate("RideOptionsCard")}>
+                    <Icon 
+                        name="car"
+                        type="font-awesome"
+                        color="white"
+                        size={16}
+                    />
+                    <Text style={tailwind`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={tailwind`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+                    <Icon 
+                        name="fast-food-outline"
+                        type="ionicon"
+                        color="black"
+                        size={16}
+                    />
+                    <Text style={tailwind`text-center`}>Eats</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
--- src/components/RideOptionsCard.js
+++ src/components/RideOptionsCard.js
@@ -1,14 +1,73 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React, { useState } from 'react'
+import { FlatList, SafeAreaView, Image, Text, TouchableOpacity, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import tailwind from 'tailwind-react-native-classnames'
+
+const data = [
+    {
+        id: 'Uber-X-123',
+        title: 'Uber X',
+        mulriplier: 1,
+        image: 'https://links.papareact.com/3pn'
+    },
+    {
+        id: 'Uber-XL-456',
+        title: 'Uber XL',
+        mulriplier: 1.2,
+        image: 'https://links.papareact.com/5w8'
+    },
+    {
+        id: 'Uber-X-789',
+        title: 'Uber LUX',
+        mulriplier: 1.75,
+        image: 'https://links.papareact.com/7pf'
+    },
+]
 
 const RideOptionsCard = () => {
+
+    const navigation = useNavigation();
+    const [selected, setSelected] = useState(null);
+
     return (
-        <View>
-            <Text>Pick a ride</Text>
-        </View>
+        <SafeAreaView style={tailwind`bg-white flex-grow`}>
+            <View>
+                <TouchableOpacity style={tailwind`absolute top-3 left-5 p-3 rounded-full z-10`} onPress={() => navigation.navigate('NavigateCard')}>
+                    <Icon 
+                        name="chevron-left"
+                        type="font-awesome"
+                    />
+                </TouchableOpacity>
+                <Text style={tailwind`text-center py-5 text-xl`}>Select a Ride</Text>
+            </View>
+
+            <FlatList 
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({item : {id, title, mulriplier, image}, item}) => (
+                    <TouchableOpacity 
+                        style={tailwind`flex-row items-center justify-between px-10 ${id === selected?.id && "bg-gray-200"}`}
+                        onPress={() => setSelected(item)}
+                    >
+                        <Image 
+                            style={{
+                                width: 100,
+                                height: 100,
+                                resizeMode: 'contain',
+                            }}
+                            source={{uri: image  }}
+                        />
+                        <View style={tailwind`-ml-6`}>
+                            <Text style={tailwind`text-xl font-semibold`}>{title}</Text>
+                            <Text>Travel time...</Text>
+                        </View>
+                        <Text style={tailwind`text-xl`}>$99</Text>
+                    </TouchableOpacity>
+                )}
+            />
+        </SafeAreaView>
     )
 }
 
 export default RideOptionsCard
-
-const styles = StyleSheet.create({})
