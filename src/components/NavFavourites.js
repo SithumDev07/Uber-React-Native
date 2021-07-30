1470c8a8a61349fabeb003e1413ffe6089ed1415
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-31 02:59:22 +0530

    Added Favourites Part

--- src/components/NavFavourites.js
+++ src/components/NavFavourites.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import tailwind from 'tailwind-react-native-classnames'
 
 const data = [
     {
@@ -21,9 +23,24 @@ const NavFavourites = () => {
         <FlatList 
             data={data}
             keyExtractor={(item) => item.id}
-            renderItem={({item}) => (
-                <TouchableOpacity>
-                    <Text>{item.location}</Text>
+            ItemSeparatorComponent={() => (
+                <View 
+                    style={[tailwind`bg-gray-200`, { height: 0.5 }]}
+                />
+            )}
+            renderItem={({item: {location, destination, icon}}) => (
+                <TouchableOpacity style={tailwind`flex-row items-center p-3`}>
+                    <Icon 
+                        style={tailwind`mr-4 rounded-full bg-gray-300 p-3`}
+                        name={icon}
+                        type="ionicon"
+                        color="white"
+                        size={18}
+                    />
+                    <View>
+                        <Text style={tailwind`font-semibold text-lg`}>{location}</Text>
+                        <Text style={tailwind`text-gray-500`}>{destination}</Text>
+                    </View>
                 </TouchableOpacity>
             )}
         />
