c1760b1a09132762417414c329f0480244f5f661
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-31 01:17:52 +0530

    Installed React Native Maps

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,8 +2,17 @@ import React from 'react'
 import { StyleSheet, Text, SafeAreaView, View, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination, selectOrigin } from '../../slices/navSlice'
+
+
 
 const HomeScreen = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -15,6 +24,36 @@ const HomeScreen = () => {
                         width: 100,
                         height: 100,
                         resizeMode: 'contain',
+                    }}
+                />
+
+                <GooglePlacesAutocomplete 
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                    placeholder="Where From?"
+                    minLength={3}
+                    onPress={(data, details = null) =>{
+                        dispatch(selectOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        }
                     }}
                 />
 
